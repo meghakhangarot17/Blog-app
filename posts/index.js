d1e7ba7aa51a1a00bd1f7b615b5003716ac155b8
meghakhangarot <meghakhangarot17@gmail.com>
2024-01-09 20:55:33 +0530

    Changes updated

---
+++ posts/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};   //store every posts that we create
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts/create", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  // await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
+});
+
+app.listen(4000, () => {
+  console.log('v10000');
+  console.log("Listening on 4000");
+});
+
+
+
+
